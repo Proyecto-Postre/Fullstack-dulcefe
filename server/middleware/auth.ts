20a1f6f81c36d96068eecf281c77abadc19911e2
Jafeth Worren Ynga Amado <jafethworren@gmail.com>
2026-07-12 00:28:54 -0500

    feat(init): base limpia de Nuxt fullstack con estructura de servidor y routing

---
+++ server/middleware/auth.ts
@@ -0,0 +1,4 @@
+export default defineEventHandler((event) => {
+  // Por ahora la aduana está abierta. Dejamos pasar la petición sin tocar nada.
+  // Mañana programaremos la lectura del Token aquí.
+})
